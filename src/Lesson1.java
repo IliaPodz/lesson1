d990a0a82650e1f62184c47bc50088425937676b
ilia <ipodzhidaev@meta-devs.io>
2021-12-21 00:56:19 +0300

    Initcommit

--- src/Lesson1.java
+++ src/Lesson1.java
@@ -1,7 +1,24 @@
-public class Lesson1 {
 
+
+import java.util.Scanner;
+
+public class Lesson1 {
     public static void main(String[] args) {
-        // write your code here
-        System.out.println("hello world");
+        //write your code here
+       //System.out.println("hello world");
+        int value1 = 0;
+        int value2 = 5;
+        int sum = value1 + value2;
+        System.out.println("Сумма " + sum);
+        int multiply = value1 * value2;
+        System.out.println("Произведение " + multiply);
+        int difference = value1 - value2;
+        System.out.println("Разность " + difference);
+        Scanner scanner = new Scanner(System.in);
+        //String userInput = scanner.next();
+        String userInput = new Scanner(System.in).next();
+        System.out.println(userInput);
+
+
     }
 }
