fbe8729c49e66d69a30591eb4fd2ddd832f8861e
ilia <ipodzhidaev@meta-devs.io>
2022-01-20 21:04:54 +0300

    lesson2

---
+++ src/Lesson2ifelse.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+import static java.lang.Integer.sum;
+
+public class Lesson2ifelse {
+    static void sum(int x, int y){
+        int z =(x+y);
+        System.out.println(z);
+    }
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 8;
+        sum(a, b); //14
+        sum(3, a); //9
+        sum(5, 23); //28
+
+        boolean aGreaterThenB = a > b;
+        boolean aLessThenB = a < b;
+        boolean aGreaterOrEqualTHenB = a >= b;
+        boolean aLessOrEqualThenB = a <= b;
+
+    if (aGreaterThenB) {
+        System.out.println("a>b");
+    }else{
+        System.out.println("False");
+    }
+    if (aLessThenB) {
+        System.out.println("a<b");
+    }
+    if (aGreaterOrEqualTHenB) {
+        System.out.println("a>=b");
+    }
+    if (aLessOrEqualThenB) {
+        System.out.println("a<=b");
+    }else {
+        System.out.println("False");
+    }
+
+    int userInput = new Scanner(System.in).nextInt();
+    if (userInput % 2 == 0) {
+        System.out.println("Число четное");
+    }else {
+        System.out.println("Число нечетное");
+    }
+    }
+}
+
+
