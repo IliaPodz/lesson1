d6bf37930da35c9950271c62ba233c2d01dc5acd
ilia <ipodzhidaev@meta-devs.io>
2022-01-24 00:41:17 +0300

    main gitignore

--- src/Lesson3.java
+++ src/Lesson3.java
@@ -1,19 +1,50 @@
-import java.util.Scanner;
 public class Lesson3 {
     public static void main(String[] args) {
-        //write your code here
-        //System.out.println("hello world");
-        int value1 = 0;
-        int value2 = 5;
-        int sum = value1 + value2;
-        System.out.println("Сумма " + sum);
-        int multiply = value1 * value2;
-        System.out.println("Произведение " + multiply);
-        int difference = value1 - value2;
-        System.out.println("Разность " + difference);
-        Scanner scanner = new Scanner(System.in);
-        //String userInput = scanner.next();
-        String userInput = new Scanner(System.in).next();
-        System.out.println(userInput);
+        int num = 0;
+        switch (num) {
+            case 1:
+                System.out.println("число равно 1");
+                break;
+            case 8:
+                System.out.println("число равно 8");
+                break;
+            case 9:
+                System.out.println("число равно 9");
+                break;
+            default:
+                System.out.println("число не равное 1,8,9");
+        }
+    }
+    public static void comparisonsOperators(int num) {
+
+        // !=-оператор сравнения "не равно"
+        // &&-быстрый оператор "и". Возвращает true только тогда, когда все выражения истинны
+        // ||- быстрый "или". Возвращает false только тогда, когда все выражения ложны
+        if (num != 8 && num != 1 && num != 9) {
+            System.out.println("Число не равно 1,8,9");
+        } else System.out.println("Число равно 1,8 или 9");
+
+        if (num == 0 || num == 1) {
+            System.out.println("Число равно 1 или 0");
+        } else {
+            System.out.println("Число не равно ни 1, ни 0");
+        }
+    }
+    public static void loops() {
+
+        int counter = 1;
+        while (counter<100) {
+            counter++;
+            System.out.println(counter);
+        }
+    }
+    public static void
+    leapyearcheck(int year){
+        while (year>=0){
+            if(year%4==0){
+                System.out.println("Високосный год:"+year);
+            }
+            year--;
+        }
     }
 }
