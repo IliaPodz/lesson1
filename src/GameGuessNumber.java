ff768f196eb1b79b48ab08a4b3863a758762de08
ilia <ipodzhidaev@meta-devs.io>
2022-02-14 20:51:59 +0300

    GameGuessNumber

--- src/GameGuessNumber.java
+++ src/GameGuessNumber.java
@@ -1,31 +1,25 @@
 import java.util.Scanner;
 
 public class GameGuessNumber {
-    public static void main(String[] args) {
-        int unknownNumber = (int) Math.floor(Math.random() * 100);
-        int NUMTRYINGS = 10;
-        boolean gameIsWon = false;
-        Scanner input = new Scanner(System.in);
-        System.out.println("ЗАДАЧА: Угадать число (от 0 до 100) за 10 попыток");
-        for (int i = 0; i < NUMTRYINGS; i++) {
-            System.out.print("Введите ваше число: ");
-            int userNumber = input.nextInt();
-            if (userNumber > unknownNumber) {
-                System.out.println("Моё число меньше.");
-            }
-            else if (userNumber < unknownNumber) {
-                System.out.println("Моё число больше.");
-            }
-            else {
-                gameIsWon = true;
-                break;
+    public static void main(String[] agrs) {
+        Scanner sc = new Scanner(System.in);
+        int a;
+        int result = (int) (Math.random() * 100+1);
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Угадайте число");
+            a = sc.nextInt();
+            if (a == result) {
+                System.out.println("Правильно");
+            } else {
+                if (a < result) {
+                    System.out.println("не правильно,Больше" + a);
+                } else {
+                    System.out.println("не правильно,Меньше" + a);
+                }
             }
         }
-        if (gameIsWon) {
-            System.out.println("Игра выиграна");
-        }
-        else {
-            System.out.println("Игра проиграна. Загаданное число " + unknownNumber);
-        }
+        System.out.println("Ответ:" + result);
+
+
     }
 }
