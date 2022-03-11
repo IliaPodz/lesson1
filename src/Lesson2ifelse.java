a93693f40fcce58f533de1fd5888b6d58e9f9d49
ilia <ipodzhidaev@meta-devs.io>
2022-03-11 22:01:18 +0300

    Lesson2ifelse

--- src/Lesson2ifelse.java
+++ src/Lesson2ifelse.java
@@ -3,10 +3,11 @@ import java.util.Scanner;
 import static java.lang.Integer.sum;
 
 public class Lesson2ifelse {
-    static void sum(int x, int y){
-        int z =(x+y);
+    static void sum(int x, int y) {
+        int z = (x + y);
         System.out.println(z);
     }
+
     public static void main(String[] args) {
         int a = 10;
         int b = 8;
@@ -19,30 +20,28 @@ public class Lesson2ifelse {
         boolean aGreaterOrEqualTHenB = a >= b;
         boolean aLessOrEqualThenB = a <= b;
 
-    if (aGreaterThenB) {
-        System.out.println("a>b");
-    }else{
-        System.out.println("False");
-    }
-    if (aLessThenB) {
-        System.out.println("a<b");
-    }
-    if (aGreaterOrEqualTHenB) {
-        System.out.println("a>=b");
-    }
-    if (aLessOrEqualThenB) {
-        System.out.println("a<=b");
-    }else {
-        System.out.println("False");
-    }
+        if (aGreaterThenB) {
+            System.out.println("a>b");
+        } else {
+            System.out.println("False");
+        }
+        if (aLessThenB) {
+            System.out.println("a<b");
+        }
+        if (aGreaterOrEqualTHenB) {
+            System.out.println("a>=b");
+        }
+        if (aLessOrEqualThenB) {
+            System.out.println("a<=b");
+        } else {
+            System.out.println("False");
+        }
 
-    int userInput = new Scanner(System.in).nextInt();
-    if (userInput % 2 == 0) {
-        System.out.println("Число четное");
-    }else {
-        System.out.println("Число нечетное");
-    }
+        int userInput = new Scanner(System.in).nextInt();
+        if (userInput % 2 == 0) {
+            System.out.println("Число четное");
+        } else {
+            System.out.println("Число нечетное");
+        }
     }
 }
-
-
